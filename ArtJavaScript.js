7e39be54ee1d850ba0c9001581d467b0f5242ddb
deocze <255413163+deocze@users.noreply.github.com>
2026-08-24 17:13:41 -0400

    Add final shadows and fix small bugs

--- ArtJavaScript.js
+++ ArtJavaScript.js
@@ -1,15 +1,3 @@
 let passwordField = document.querySelector("#password");
 let confirmPasswordField = document.querySelector("#passwordConfirmed");
 
-confirmPasswordField.addEventListener("keydown", (e) =>
-  checkPass(passwordField, confirmPasswordField),
-);
-
-function checkPass(oPass, cPass) {
-  console.log("Running");
-  let originalPass = oPass.value;
-  let confirmedPass = cPass.value;
-  if (originalPass === confirmedPass) {
-    console.log("Yay");
-  }
-}
